f35674a17e32bf8cc0fadeafaf7b7c2ce97291eb
BetLopGlz <84944622+BetLopGlz@users.noreply.github.com>
2021-07-29 18:32:56 -0500

    agregando formulario para obtener datos de tarea

---
+++ agenda/index.js
@@ -0,0 +1,6 @@
+guardar.addEventListener("click", function () {
+    let tarea=document.getElementById("tarea").value;
+    let descripcion=document.getElementById("descripcion").value;
+    console.log(tarea);
+    console.log(descripcion);
+});
